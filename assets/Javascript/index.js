8c3df9ee8cae9373624e43e5a297b2d1491187f7
Morenike Oyewole <morenikeoyewole@Morenikes-MacBook-Pro.local>
2021-11-05 11:51:57 +0100

    new navs

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -1,9 +1,10 @@
 const toggle = () => {
-  const burger = document.querySelector(".toggler");
-  const nav = document.querySelector(".nav-links");
+  const burger = document.querySelector(".toggle-btn");
+  const nav = document.querySelector(".navbar-nav");
   const navItems = document.querySelectorAll(".nav-link");
+
   burger.addEventListener("click", () => {
-    nav.classList.toggle("show");
+    nav.classList.remove("show");
   });
   for (const navItem of navItems) {
     navItem.addEventListener("click", () => {
@@ -12,3 +13,13 @@ const toggle = () => {
   }
 };
 toggle();
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
